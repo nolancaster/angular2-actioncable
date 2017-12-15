86ec1524a2d1f9320ba8e51a09be66b46ce9a984
Nolan Lancaster <nolancaster@gmail.com>
2017-12-15 00:42:38 -0500

    Unsubcribe from channel on unsubscribe from messages observable

--- src/cable.ts
+++ src/cable.ts
@@ -1,6 +1,5 @@
 import * as ActionCableNs from 'actioncable';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
 
 const ActionCable = ActionCableNs;
 
@@ -16,15 +15,7 @@ export class Cable {
    * Create a subscription to a channel or return the existing one
    */
   channel(name: string, params = {}): Channel {
-    let channel = this.findChannel(name, params);
-    if (!channel) {
-      channel = new Channel(this, name, params);
-    }
-    return channel
-  }
-
-  findChannel(name: string, params = {}) {
-    return this.channels.find(channel => channel.isFor(name, params));
+    return new Channel(this, name, params);
   }
 
   disconnect() {
@@ -33,14 +24,16 @@ export class Cable {
 }
 
 export class Channel {
-  private source: Subject<any> = new Subject();
-  messages: Observable<any> = this.source.asObservable();
+  messages: Observable<any>;
   baseChannel: any;
 
   constructor(public cable: Cable, public name: string, public params = {}) {
     const channelParams = Object.assign({}, params, {channel: name});
-    this.baseChannel = this.cable.baseCable.subscriptions.create(channelParams, {
-      received: (data: any) => this.source.next(data)
+    this.messages = Observable.create(observer => {
+      this.baseChannel = this.cable.baseCable.subscriptions.create(channelParams, {
+        received: (data: any) => observer.next(data)
+      });
+      return () => this.unsubscribe();
     });
   }
 
@@ -50,27 +43,5 @@ export class Channel {
 
   unsubscribe() {
     this.cable.baseCable.subscriptions.remove(this.baseChannel);
-    this.source.complete();
-  }
-
-  isFor(name: string, params = {}) {
-    if (this.name !== name) {
-      return false;
-    }
-
-    const theseParams = Object.getOwnPropertyNames(this.params);
-    const thoseParams = Object.getOwnPropertyNames(params);
-
-    if (theseParams.length !== thoseParams.length) {
-      return false;
-    }
-
-    for (const param of theseParams) {
-      if (theseParams[param] !== thoseParams[param]) {
-        return false;
-      }
-    }
-
-    return true;
   }
 }
